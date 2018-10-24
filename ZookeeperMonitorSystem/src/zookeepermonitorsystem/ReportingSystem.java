d2e8a34768ac2bcc5aee897e8a48b2975913aab9
Jeremy <42558493+jsmiller890@users.noreply.github.com>
2018-10-23 22:50:25 -0400

    Update ReportingSystem.java

--- ZookeeperMonitorSystem/src/zookeepermonitorsystem/ReportingSystem.java
+++ ZookeeperMonitorSystem/src/zookeepermonitorsystem/ReportingSystem.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package zookeepermonitorsystem;
 
 import java.io.File;
@@ -14,8 +9,7 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
- *
- * @author cerul
+ * Author Jeremy Miller
  */
 public class ReportingSystem {
     
