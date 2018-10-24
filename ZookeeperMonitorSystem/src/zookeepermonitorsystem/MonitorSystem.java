b4b751b7663c3f5f1e958ccc38c939a4985ea13f
Jeremy <42558493+jsmiller890@users.noreply.github.com>
2018-10-23 22:49:45 -0400

    Update MonitorSystem.java

--- ZookeeperMonitorSystem/src/zookeepermonitorsystem/MonitorSystem.java
+++ ZookeeperMonitorSystem/src/zookeepermonitorsystem/MonitorSystem.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package zookeepermonitorsystem;
 
 import java.io.IOException;
 import java.util.Scanner;
 
 /**
- *
- * @author cerul
+ * Author Jeremy Miller
  */
 public class MonitorSystem {
     public static void RunMonitor(){
